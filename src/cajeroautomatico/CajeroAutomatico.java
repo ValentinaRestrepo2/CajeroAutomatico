1a63e8021421ab9506f072f1f4cf8dcc7d74b398
Valentina Restrepo <73320777+ValentinaRestrepo2@users.noreply.github.com>
2022-11-17 22:08:30 -0500

    Cambios en algunos ciclos

--- src/cajeroautomatico/CajeroAutomatico.java
+++ src/cajeroautomatico/CajeroAutomatico.java
@@ -15,36 +15,48 @@ public class CajeroAutomatico {
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
         System.out.println("Ingrese el valor a retirar del cajero: ");
-        int monto = scaner.nextInt();
+        int monto = scaner.nextInt(); //Se captura el monto a retirar 
         CajeroAutomatico retirocajero = new CajeroAutomatico();
         retirocajero.RetirarDinero(monto);
     }
 
+    /**
+     * @param monto el valor a retirar del cajero
+     */
     public void RetirarDinero(int monto) {
-        int[] billetesDisponibles = {50000, 20000, 10000};
-        ArrayList billetesdevuetos = new ArrayList();
+        int[] denominacionBilletes = {50000, 20000, 10000};
         int xbillete = 0, contador = 0, sumaMonto = 0, aux = 0;
         int diferencia = monto;
+        ArrayList billetesdevuetos = new ArrayList();
 
-        if (monto % 10000 == 0) {
+        if (monto % 10000 == 0) { // Valida que el monto sea múltiplo de 10.000
             while (sumaMonto != monto) {
                 diferencia -= xbillete;
-                xbillete = SeleccionarBillete(billetesDisponibles, diferencia, aux);
+                xbillete = SeleccionarBillete(denominacionBilletes, diferencia, aux);
                 sumaMonto += xbillete;
                 billetesdevuetos.add(xbillete);
                 contador++;
             }
-            for (int j = 0; j < billetesdevuetos.size(); j++) {
-                System.out.println(billetesdevuetos.get(j));
+            for (Object billetesdevueto : billetesdevuetos) {
+                System.out.println("Billete de " + billetesdevueto);
+
             }
             System.out.println("El retiro de " + sumaMonto + " fue exitoso");
         }
     }
 
-    int SeleccionarBillete(int[] billetesDisponibles, int diferencia, int aux) {
-        for (int i = 0; i < billetesDisponibles.length; i++) {
-            if (billetesDisponibles[i] <= diferencia) {
-                aux = billetesDisponibles[i];
+    /**
+     * @param billetesDisponibles vector con las denominaciones de billetes que
+     * maneja el cajero (10.000,20.000,50.000)
+     * @param diferencia valor que existe entre el monto y sumaMonto, es decir
+     * lo que resta para completar el retiro
+     * @param aux variable auxiliar que almacena el billete que más se adapta o
+     * se acerca a la diferencia
+     */
+    public int SeleccionarBillete(int[] denominacionBilletes, int diferencia, int aux) {
+        for (int i = 0; i < denominacionBilletes.length; i++) {
+            if (denominacionBilletes[i] <= diferencia) {
+                aux = denominacionBilletes[i];
                 break;
             }
         }
